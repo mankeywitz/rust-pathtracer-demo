f4f34118e6b13d760953a26c7a05fc4ee30f05ba
Luna <14264545+mankeywitz@users.noreply.github.com>
2022-05-14 16:21:05 -0500

    testing that image generation works

--- src/main.rs
+++ src/main.rs
@@ -11,93 +11,17 @@ mod vec3;
 use vec3::Vec3;
 
 mod ray;
-use ray::Ray;
-
 mod hitable;
-use hitable::Hitable;
-use hitable::BvhNode;
-
 mod sphere;
-use sphere::Sphere;
-
 mod triangle;
-use triangle::Triangle;
-
 mod material;
-use material::{DiffuseLight, Lambertian};
-
 mod camera;
-use camera::Camera;
-
 mod texture;
-use texture::ConstantTexture;
-
 mod aabb;
-
-extern crate rayon;
-use rayon::prelude::*;
-
-extern crate obj;
-use obj::Obj;
 
 extern crate clap;
 use clap::{Arg, App};
-
-fn triangulate(vertices: Vec<Vec3>) -> Vec<Box<dyn Hitable + Sync>> {
-    assert!(vertices.len() >= 3, "Input face must have at least 3 vertices!");
-    let mut output: Vec<Box<dyn Hitable + Sync>> = Vec::new();
-
-    //Trivial case: exactly 3 vertices are passed in
-    if vertices.len() == 3 {
-        let edge1 = vertices[1] - vertices[0];
-        let edge2 = vertices[2] - vertices[0];
-        let normal = Vec3::unit_vector(edge1.cross(edge2));
-        let material = Lambertian::new(Box::new(ConstantTexture::new(Vec3::new(1.0, 1.0, 1.0))));
-        output.push(Box::new(Triangle::new(vertices[0], vertices[1], vertices[2], normal, Box::new(material))));
-    } else { //Non trivial case - parse vertices as triangle fan
-        let common_idx = 0;
-        let mut first_idx = 1;
-        let mut second_idx = 2;
-
-        let common_v = vertices[common_idx];
-
-        while second_idx < vertices.len() {
-            let v1 = vertices[first_idx];
-            let v2 = vertices[second_idx];
-            let edge1 = v1 - common_v;
-            let edge2 = v2 - common_v;
-            let normal = Vec3::unit_vector(edge1.cross(edge2));
-            let material = Lambertian::new(Box::new(ConstantTexture::new(Vec3::new(1.0, 1.0, 1.0))));
-            output.push(Box::new(Triangle::new(common_v, v1, v2, normal, Box::new(material))));
-            first_idx = first_idx + 1;
-            second_idx = second_idx + 1;
-        }
-    }
-    output
-}
-
-fn color(r : &Ray, world: &Box<dyn Hitable + Sync>, depth: u32) -> Vec3 {
-    let hit_rec = world.hit(0.001, 50.0, r);
-    if hit_rec.is_some() {
-        let hit_rec = hit_rec.unwrap();
-        let material = hit_rec.material;
-        let normal = hit_rec.normal;
-        let point = hit_rec.p;
-        let t = hit_rec.t;
-        let scatter_rec = material.scatter(r, t, point, normal);
-        let emitted = material.emitted(0.0, 0.0, &point);
-        if scatter_rec.is_some() && depth < 50 {
-            let scatter_rec = scatter_rec.unwrap();
-            return emitted + scatter_rec.attenuation * color(&scatter_rec.scattered, world, depth + 1);
-        } else {
-            return emitted;
-        }
-    }
-    Vec3::new(0.0, 0.0, 0.0)
-    //let unit_direction = Vec3::unit_vector(r.direction());
-    //let t = 0.5 * (unit_direction.y() + 1.0);
-    //(1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
-}
+ 
 
 fn main() {
     //Setup args
@@ -138,34 +62,6 @@ fn main() {
 
     println!("Generating a {}x{}@{}spp render of {}, saving to {}", image_width, image_height, samples_per_pixel, filename, output_filename);
 
-    //Generate world
-    let mut world: Vec<Box<dyn Hitable + Sync>> = Vec::new();
-
-    let obj_file = Obj::load(Path::new(filename)).unwrap();
-    for object in obj_file.data.objects.iter() {
-        for group in object.groups.iter() {
-            for polygon in group.polys.iter() {
-                let mut vertices: Vec<Vec3> = Vec::new();
-                for vertex in polygon.0.iter() {
-                    let index = vertex.0;
-                    let position = obj_file.data.position[index];
-                    vertices.push(Vec3::new(position[0], position[1], position[2]));
-                }
-                world.append(&mut triangulate(vertices));
-            }
-        }
-    }
-
-    world.push(Box::new(Sphere::new(Vec3::new(0.0, 0.0, 2.0), 0.5, Box::new(DiffuseLight::new(Box::new(ConstantTexture::new(Vec3::new(2.0, 2.0, 2.0))))))));
-
-    let bvh_tree: Box<dyn Hitable + Sync> = Box::new(BvhNode::new(world));
-    //Setup camera
-    let lookfrom = 3.0 * Vec3::new(-2.26788425, 0.320256859, 1.83503199);
-    let lookat = Vec3::new(-1.33643341, 0.320256859, 1.47116470);
-    let focus_dist = (lookfrom - lookat).length();
-    let aperture = 0.0;
-    let camera = Camera::new(lookfrom, lookat, Vec3::new(0.0, 1.0, 0.0), 20.0, image_width as f32 / image_height as f32, aperture, focus_dist);
-
     //Generate image
     let mut data = Vec::new();
 
@@ -174,31 +70,12 @@ fn main() {
 
     for y in (0..image_height).rev() {
         for x in 0..image_width {
-            let mut samples = vec![Vec3::zero_vector(); samples_per_pixel];
 
-            samples.par_iter_mut().for_each( |sample| {
-                let u = (x as f32 + rand::random::<f32>()) / image_width as f32;
-                let v = (y as f32 + rand::random::<f32>()) / image_height as f32;
+            let color = Vec3::new(x as f32 / image_width as f32, y as f32 / image_height as f32, 0.0);
 
-                let r = camera.get_ray(u, v);
-                *sample = color(&r, &bvh_tree, 0);
-            });
-
-            let mut avg_color = Vec3::zero_vector();
-
-            samples.iter().for_each( |sample| {
-                avg_color = avg_color + *sample;
-            });
-
-            avg_color = avg_color / samples_per_pixel as f32;
-
-            //Do gamma correction
-            avg_color = avg_color.clamp(Vec3::new(0.0, 0.0, 0.0), Vec3::new(1.0, 1.0, 1.0));
-            avg_color = Vec3::new(avg_color.x().sqrt(), avg_color.y().sqrt(), avg_color.z().sqrt());
-
-            let ir = (255.99*avg_color.x()) as u8;
-            let ig = (255.99*avg_color.y()) as u8;
-            let ib = (255.99*avg_color.z()) as u8;
+            let ir = (255.99*color.x()) as u8;
+            let ig = (255.99*color.y()) as u8;
+            let ib = (255.99*color.z()) as u8;
 
             data.push(ir);
             data.push(ig);
